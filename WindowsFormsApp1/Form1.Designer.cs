4d71cfee638a9b028b37e65489162c2ab82d9121
yos <shi1friver@yahoo.co.jp>
2019-12-01 15:53:36 +0900

    edti2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -142,7 +142,7 @@
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip2;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "かざぐるマウスぽいもの";
+            this.notifyIcon1.Text = "ウィンドウ操作";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.DoubleClick += new System.EventHandler(this.NotifyIcon1_DoubleClick);
             // 
