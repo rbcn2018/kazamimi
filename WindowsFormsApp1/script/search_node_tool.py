8979e3f86f6fd42e29735d048e5d9cb64eef93fa
yos <shi1friver@yahoo.co.jp>
2020-03-09 01:05:13 +0900

    edit

--- WindowsFormsApp1/script/search_node_tool.py
+++ WindowsFormsApp1/script/search_node_tool.py
@@ -5,31 +5,29 @@ import search_node_tool
 reload(search_node_tool)
 search_node_tool.SearchNodeTool().show_ui()
 
-Starts with(Default)
+
+Partial Match(Default)
     e.g.:
-        text:joint1
+        text:joint
+        match:hip_joint,spinejoint_end,...
+
+Starts with
+    There is "^" at the start.
+    e.g.:
+        text:^joint1
         match:joint1,joint10,...
 
-Perfect match
+Backward match
     There is "$" at the end.
     e.g.:
         text:joint1$
-        match:joint1
-
-Backward match
-    There is "*" at the start and "$" at the end.
-    e.g.:
-        text:*joint1$
         match:joint1,LeftArm_joint1,
 
-Partial Match
-    There is "*" at the start.
+Perfect match
+    There is "^" at the start and "$" at the end.
     e.g.:
-        text:*joint
-        match:hip_joint,spinejoint_end,...
-
-TODO:
-    Exclude namespace (checkbox)
+        text:*joint1$
+        match:joint1
 
 """
 
@@ -45,76 +43,81 @@ class SearchNodeTool():
         with pc.window(wname, maximizeButton=False, minimizeButton=False):
             with pc.formLayout() as form1:
                 self.txt_ui = pc.textField(textChangedCommand=self.change_condition)
-                self.case_chk_ui = pc.checkBox(label='Recognize case', changeCommand=self.change_condition)
-                self.all_chk_ui = pc.checkBox(label='Include all types', changeCommand=self.change_condition)
+                self.ex_ns_chk_ui = pc.checkBox(label='Exclude namespace', changeCommand=self.change_condition)#, value=True)
+                self.rec_case_chk_ui = pc.checkBox(label='Recognize case', changeCommand=self.change_condition)
+                self.alltype_chk_ui = pc.checkBox(label='Include all types', changeCommand=self.change_condition)
                 self.node_lst_ui = pc.textScrollList(allowMultiSelection=True, selectCommand=self.change_select)
                 aflst = [[self.node_lst_ui, 'bottom', 0],
                          [self.node_lst_ui, 'left', 0],
                          [self.node_lst_ui, 'right', 0],
                          [self.txt_ui, 'left', 0],
                          [self.txt_ui, 'right', 0],
-                         [self.case_chk_ui, 'left', 0],
-                         [self.case_chk_ui, 'right', 0],
-                         [self.all_chk_ui, 'left', 0],
-                         [self.all_chk_ui, 'right', 0],
                          ]
                 aclst = [[self.node_lst_ui, 'top', 0, self.txt_ui],
-                         [self.txt_ui, 'top', 0, self.all_chk_ui],
-                         [self.all_chk_ui, 'top', 0, self.case_chk_ui],
+                         [self.txt_ui, 'top', 0, self.rec_case_chk_ui],
+                         [self.rec_case_chk_ui, 'top', 0, self.alltype_chk_ui],
+                         [self.alltype_chk_ui, 'top', 0, self.ex_ns_chk_ui],
                          ]
                 pc.formLayout(form1, e=True, attachForm=aflst, attachControl=aclst)
 
-        typ = dict() if self.all_chk_ui.getValue() else dict(type='transform')
+        all_typ = self.alltype_chk_ui.getValue()
+        typ = dict() if all_typ else dict(type='transform')
         all_nodes = cmds.ls(**typ)
-        for node in all_nodes:
+        if not all_typ:
+            ex_type = cmds.ls(all_nodes, type=['animCurve', 'constraint'])
+            all_nodes = list(set(all_nodes) - set(ex_type))
+        for node in sorted(all_nodes):
             self.node_lst_ui.append(node)
 
     def change_condition(self, *arg):
-        status = 'Starts'
-        pfx = ''
-        sfx = '*'
+        status = 'Partial'
         txt = self.txt_ui.getText()
-        if txt.startswith('*') and txt.endswith('$'):
-            status == 'Ends'
-            pfx = '*'
-            sfx = ''
+        if txt.startswith('^') and txt.endswith('$'):
+            status = 'Perfect'
             txt = txt[1:-1]
-        elif txt.startswith('*'):
-            status = 'Partial'
-            pfx = '*'
-            sfx = '*'
+        elif txt.startswith('^'):
+            status = 'Starts'
             txt = txt[1:]
         elif txt.endswith('$'):
-            status = 'Perfect'
-            pfx = ''
-            sfx = ''
+            status == 'Ends'
             txt = txt[:-1]
 
         self.node_lst_ui.removeAll()
 
-        typ = dict() if self.all_chk_ui.getValue() else dict(type='transform')
+        all_typ = self.alltype_chk_ui.getValue()
+        case_chk = self.rec_case_chk_ui.getValue()
+        ex_ns = self.ex_ns_chk_ui.getValue()
 
-        if not self.case_chk_ui.getValue():
-            fil_nodes = cmds.ls(pfx + txt + sfx, **typ)
-            for node in fil_nodes:
+        typ = dict() if all_typ else dict(type='transform')
+        txt2 = txt if case_chk else txt.lower()
+        all_nodes = cmds.ls(**typ)
+        if not all_typ:
+            ex_type = cmds.ls(all_nodes, type=['animCurve', 'constraint'])
+            all_nodes = list(set(all_nodes) - set(ex_type))
+        for node in sorted(all_nodes):
+            if not txt2:
                 self.node_lst_ui.append(node)
-        else:
-            txt_low = txt.lower()
-            all_nodes = cmds.ls(**typ)
-            for node in all_nodes:
-                node_low = node.lower()
-                if status == 'Partial':
-                    if txt_low in node_low:
-                        self.node_lst_ui.append(node)
-                elif status == 'Perfect':
-                    if txt_low == node_low:
-                        self.node_lst_ui.append(node)
-                elif status == 'Starts':
-                    if node_low.startswith(txt_low):
-                        self.node_lst_ui.append(node)
-                elif status == 'Ends':
-                    if node_low.endswith(txt_low):
-                        self.node_lst_ui.append(node)
+                continue
+
+            node2 = node if case_chk else node.lower()
+            if ex_ns:
+                node2 = node2.rsplit(':')[-1]
+
+            if status == 'Partial':
+                if txt2 in node2:
+                    self.node_lst_ui.append(node)
+
+            elif status == 'Perfect':
+                if txt2 == node2:
+                    self.node_lst_ui.append(node)
+
+            elif status == 'Starts':
+                if node2.startswith(txt2):
+                    self.node_lst_ui.append(node)
+
+            elif status == 'Ends':
+                if node2.endswith(txt2):
+                    self.node_lst_ui.append(node)
 
     def change_select(self):
         selected = self.node_lst_ui.getSelectItem()
